f49de45fe727f275b0d8359c1da1bb7b46b1d927
chriscotimms <chriscotimms@gmail.com>
2024-01-02 09:37:03 +0000

    first challenge passed

--- src/server.js
+++ src/server.js
@@ -2,4 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.listen(3000, () => console.log("Listening at http://localhost:3000"));
+
+server.get("/", (request, response) => {
+  response.send(`<h1>Hello Express</h1>`);
+});
+
 module.exports = server;
