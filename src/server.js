efa9f4f41b0133e575b2fe2df57ff2dea3bb459a
chriscotimms <chriscotimms@gmail.com>
2024-01-02 10:46:25 +0000

    colour challenge working

--- src/server.js
+++ src/server.js
@@ -2,10 +2,33 @@ const express = require("express");
 
 const server = express();
 
-server.listen(3000, () => console.log("Listening at http://localhost:3000"));
-
 server.get("/", (request, response) => {
   response.send(`<h1>Hello Express</h1>`);
 });
+
+server.get("/colour", (request, response) => {
+    const keyword = request.query;
+    const colour = keyword.hex || 'FFFFFF';
+    console.log(keyword.hex, colour);
+   
+    response.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    body {
+      background-color: #${colour};
+    }
+    </style>
+    </head>
+    <body>
+    
+    </body>
+    </html> 
+    `);
+        // response.status(200).send("something went wrong");
+    
+    
+  });
 
 module.exports = server;
