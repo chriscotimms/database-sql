085f646de703f6ae591296d678c3d93d4f524ddb
chriscotimms <chriscotimms@gmail.com>
2024-01-02 11:54:32 +0000

    added hex picker challenge 3 completed

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ server.get("/", (request, response) => {
 
 server.get("/colour", (request, response) => {
     const keyword = request.query;
-    const colour = keyword.hex || 'FFFFFF';
+    const colour = keyword.hex || '#FFFFFF';
     console.log(keyword.hex, colour);
    
     response.send(`
@@ -17,16 +17,27 @@ server.get("/colour", (request, response) => {
     <head>
     <style>
     body {
-      background-color: #${colour};
+      background-color: ${colour};
     }
     </style>
     </head>
     <body>
-    
+    <form action="/colour" method="GET">
+    <li>
+      <label for="name">choose a hex colour #</label>
+   
+      <input type="color" id="head" name="hex" value="#e66465" />
+    </li>
+    <li class="button">
+  <button type="submit">Send your message</button>
+    </li>
+    </form>
+
     </body>
     </html> 
     `);
         // response.status(200).send("something went wrong");
+         //   <input type="text" id="hex" name="hex" />
     
     
   });
